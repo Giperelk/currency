c83aeb4c0eba7687769fde5046a524cf28b90b73
Giperelk <mizerisiad1722@gmail.com>
2023-03-06 18:19:11 +0200

    HW10

--- Makefile
+++ Makefile
@@ -10,5 +10,11 @@ migrate:
 makemigrations:
 	$(manage) makemigrations
 
+createsuperuser:
+	$(manage) createsuperuser
+
 shell:
 	$(manage) shell_plus --print-sql
+
+flake8:
+	flake8 app
--- app/currency/admin.py
+++ app/currency/admin.py
@@ -1,3 +1,50 @@
-# from django.contrib import admin
+from django.contrib import admin
+from currency.models import Rate, ContactUs, Source
+from rangefilter.filters import DateRangeFilter
+from import_export.admin import ImportExportModelAdmin
 
-# Register your models here.
+
+@admin.register(Rate)
+class RateAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'currency',
+        'buy',
+        'sell',
+        'source',
+        'created',
+    )
+    list_filter = (
+        'currency',
+        ('created', DateRangeFilter),
+    )
+    search_fields = (
+        'source',
+        'currency',
+    )
+
+
+@admin.register(ContactUs)
+class ContactUsAdmin(ImportExportModelAdmin):
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+    def has_add_permission(self, request):
+        return False
+
+
+@admin.register(Source)
+class SourceAdmin(ImportExportModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'source_url',
+        'phone_number',
+        'contact_email',
+    )
+    search_fields = (
+        'name',
+    )
